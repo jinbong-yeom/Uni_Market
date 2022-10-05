fbe0838345ae8c06e29f257f63c8ee2485487a6a
jinbong <wlftj13@naver.com>
2022-10-05 16:29:34 +0900

    Fix DBNAME

--- DataBase/unimarket.py
+++ DataBase/unimarket.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 client=MongoClient(host='localhost',port=27017)
 print(client.list_database_names())
-db=client['test']
+db=client['UniMarketDB']
 collection=db['data']
 posts=db.data
 db.list_collection_names()
