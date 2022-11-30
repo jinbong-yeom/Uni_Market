cc205d19e6a7cfe6732aad5e35a16de08a42554e
kiwi_j500 <mingureion@gmail.com>
2022-12-01 04:59:57 +0900

    핸들러추가, 로딩시간 2초로 설정

--- unimarket-app/android/app/src/main/java/com/example/unimarket/IntroActivity.java
+++ unimarket-app/android/app/src/main/java/com/example/unimarket/IntroActivity.java
@@ -13,5 +13,15 @@ public class IntroActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
 
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
+
     }
 }
