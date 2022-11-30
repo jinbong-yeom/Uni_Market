6d95f04749552f1e5ea3d96e48c990fdfc927db5
kiwi_j500 <mingureion@gmail.com>
2022-12-01 04:56:48 +0900

    인트로 소스

---
+++ unimarket-app/android/app/src/main/java/com/example/unimarket/IntroActivity.java
@@ -0,0 +1,17 @@
+package com.example.unimarket;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+    }
+}
