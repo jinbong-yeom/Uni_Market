28b3450c798e96b2fdb596e308c9bcdabcd60f77
kiwi_j500 <mingureion@gmail.com>
2022-12-02 07:02:07 +0900

    리스너 등록

--- unimarket-app/android/app/src/main/java/com/example/unimarket/Fragment1.java
+++ unimarket-app/android/app/src/main/java/com/example/unimarket/Fragment1.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,7 +13,23 @@ public class Fragment1 extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup frag1V = (ViewGroup) inflater.inflate(R.layout.fragment1, container, false);
+
+        SearchView searchBar = frag1V.findViewById(R.id.searchView1);
+        searchBar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                // 입력받은 문자열 처리
+                Toast.makeText(getActivity(),s, Toast.LENGTH_LONG).show();
+                return true;
+            }
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
         return inflater.inflate(R.layout.fragment1, container, false);
     }
-
+    
 }
