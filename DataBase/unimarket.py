1ee10cbc43e4430fd94c6e07a8c3bfef7591f468
jinbong <wlftj13@naver.com>
2022-12-02 20:02:05 +0900

    제외할 단어 필터링 추가

--- DataBase/unimarket.py
+++ DataBase/unimarket.py
@@ -3,11 +3,16 @@ from pymongo import MongoClient
 uri = "" % (
                 '', '', '')
 client=MongoClient(uri)
+
 db=client['UniMarketDB']
 collection=db['data']
 sanghan=300000
-hahan=100000
-title="아이폰"
-for i in collection.find({'$and':[{"price":{"$lte":sanghan}},{"price":{"$gte":hahan}},
-{"title":{"$regex":".*{}.*".format(title)}}]}).sort("price"):
+hahan=0
+title="갤럭시"
+one="S21"
+two="S7"
+three="무고"
+for i in collection.find({'$and':[{'$and':[{"price":{"$lte":sanghan}},{"price":{"$gte":hahan}},
+{"title":{"$regex":".*{}.*".format(title)}}]},{'$nor':[{"title":{"$regex":".*{}.*".format(one)}},
+{"title":{"$regex":".*{}.*".format(two)}},{"title":{"$regex":".*{}.*".format(three)}}]}]}).sort("price"):
       print(i)
