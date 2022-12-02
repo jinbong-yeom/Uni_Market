e27526920fac504c41d0fdaa8520146c64a66486
jinbong <wlftj13@naver.com>
2022-12-02 22:07:01 +0900

    알림 보내기 초안

--- DataBase/unimarket.py
+++ DataBase/unimarket.py
@@ -1,7 +1,8 @@
-from pymongo import MongoClient
+from pymongo import MongoClient 
+import json
 
-uri = "" % (
-                '', '', '')
+uri = "mongodb://%s:%s@%s/?authMechanism=DEFAULT&authSource=UniMarketDB" % (
+                'uni', 'uni1234', 'db.yoonleeverse.com')
 client=MongoClient(uri)
 
 db=client['UniMarketDB']
@@ -12,6 +13,14 @@ title="갤럭시"
 one="S21"
 two="S7"
 three="무고"
+item_id=[]# 기존 item_id
+for i in collection.find():
+      item_id.append(i['item_id'])
+      
+for i in collection.find():#갱신된 부분이 있는지 확인
+      if i['item_id'] not in item_id:
+            print('알림')
+            # 알림 보내기
 for i in collection.find({'$and':[{'$and':[{"price":{"$lte":sanghan}},{"price":{"$gte":hahan}},
 {"title":{"$regex":".*{}.*".format(title)}}]},{'$nor':[{"title":{"$regex":".*{}.*".format(one)}},
 {"title":{"$regex":".*{}.*".format(two)}},{"title":{"$regex":".*{}.*".format(three)}}]}]}).sort("price"):
