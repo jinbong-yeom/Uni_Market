b18efe5f8a6f767657cd7a58fe17cc81e94d31c6
stoneTiger0912 <seokbeom0912@gmail.com>
2022-12-03 20:09:12 +0900

    Add PostData_filtering

--- unimarket-app/android/app/src/main/java/com/example/unimarket/PostData.java
+++ unimarket-app/android/app/src/main/java/com/example/unimarket/PostData.java
@@ -2,6 +2,8 @@ package com.example.unimarket;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.List;
+
 public class PostData {
     @SerializedName("title")
     private String title;
@@ -9,16 +11,25 @@ public class PostData {
     @SerializedName("userId")
     private String userId;
 
+    @SerializedName("filteringData")
+    public FilteringData filteringData;
+
     public String getTitle() {
         return title;
     }
+
 
     public String getUserId() {
         return userId;
     }
 
-    public PostData(String title, String userId) {
+    public FilteringData getFilteringData() {
+        return filteringData;
+    }
+
+    public PostData(String title, String userId, FilteringData filteringData) {
         this.title = title;
         this.userId = userId;
+        this.filteringData = filteringData;
     }
 }
