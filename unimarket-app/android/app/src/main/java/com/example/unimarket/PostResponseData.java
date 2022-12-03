4f89e77a26cf7ae1e63ae84a92a74ae0aad85d90
stoneTiger0912 <seokbeom0912@gmail.com>
2022-12-03 18:00:14 +0900

    Add PostResponse

---
+++ unimarket-app/android/app/src/main/java/com/example/unimarket/PostResponseData.java
@@ -0,0 +1,25 @@
+package com.example.unimarket;
+
+import com.google.gson.annotations.SerializedName;
+
+public class PostResponseData {
+
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("picture")
+    private String picture;
+
+    @SerializedName("region")
+    private String region;
+
+    @SerializedName("price")
+    private int price;
+
+    @SerializedName("link")
+    private String link;
+
+    @SerializedName("app_name")
+    private String app_name;
+    
+}
