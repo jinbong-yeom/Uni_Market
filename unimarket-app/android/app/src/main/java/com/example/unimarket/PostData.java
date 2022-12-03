b9a88b66e9f9715ef5b32a8a24659e735ca5094e
stoneTiger0912 <seokbeom0912@gmail.com>
2022-12-03 17:58:05 +0900

    Add PostData

---
+++ unimarket-app/android/app/src/main/java/com/example/unimarket/PostData.java
@@ -0,0 +1,12 @@
+package com.example.unimarket;
+
+import com.google.gson.annotations.SerializedName;
+
+public class PostData {
+    @SerializedName("title")
+    private String title;
+
+    @SerializedName("userId")
+    private String userId;
+
+}
