7d08fda2775b82efdb1de5e22a92900f7a8e6e92
15zd <mingureion@gmail.com>
2022-12-03 22:48:38 +0900

    검색창에서 키워드 넘겨받아서 알림창에서 업데이트

--- unimarket-app/android/app/src/main/java/com/example/unimarket/Fragment2.java
+++ unimarket-app/android/app/src/main/java/com/example/unimarket/Fragment2.java
@@ -16,6 +16,12 @@ public class Fragment2 extends Fragment {
         ViewGroup frag2V = (ViewGroup) inflater.inflate(R.layout.fragment3, container, false);
         TextView nkeyword = frag2V.findViewById(R.id.frag2_textView2);
 
+        if (getArguments() != null) {
+            trans = getArguments().getString("srinput"); // 프래그먼트1에서 받아온 값 넣기
+            //Toast.makeText(getActivity(),trans +"넘겨받은 문자열", Toast.LENGTH_LONG).show();
+            nkeyword.setText(trans);
+        }
+
         return frag2V;
     }
 }
