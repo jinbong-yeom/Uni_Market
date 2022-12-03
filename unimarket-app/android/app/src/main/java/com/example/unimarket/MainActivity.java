bdcf643f8bbaf7a5144ce9e9d97e662367e840e0
15zd <mingureion@gmail.com>
2022-12-04 06:06:03 +0900

    프래그먼트 전환방식 수정

--- unimarket-app/android/app/src/main/java/com/example/unimarket/MainActivity.java
+++ unimarket-app/android/app/src/main/java/com/example/unimarket/MainActivity.java
@@ -11,8 +11,8 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainActivity extends AppCompatActivity {
 
     Fragment1 fragment1;
-    Fragment2 fragment2;
-    Fragment3 fragment3;
+//    Fragment2 fragment2;
+//    Fragment3 fragment3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +20,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //프래그먼트생성
-        fragment1 = new Fragment1();    // new 연산자 사용하더라도 바로 프래그먼트로 동작하는건 아님
-        fragment2 = new Fragment2();
-        fragment3 = new Fragment3();
+//        fragment1 = new Fragment1();    // new 연산자 사용하더라도 바로 프래그먼트로 동작하는건 아님
+//        fragment2 = new Fragment2();
+//        fragment3 = new Fragment3();
 
         //처음엔 검색탭에서 시작
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -34,18 +34,47 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
-                    case R.id.search1:
-                            fragmentManager.beginTransaction()
-                                .replace(R.id.main_frame, fragment1).commit();
+                    case R.id.search1: { //검색 탭이 선택됬을 때
+                        // 프래그먼트가 이미 존재하면 보여주고 아니면 프래그먼트 매니저에 추가
+                        if (fragmentManager.findFragmentByTag("search1") != null)
+                            fragmentManager.beginTransaction().show(fragmentManager.findFragmentByTag("search1")).commit();
+                        else
+                            fragmentManager.beginTransaction().add(R.id.main_frame, new Fragment1(), "search1").commit();
+                        //나머지 프래그먼트가 활성화 됬을 때 가려준다.
+                        if (fragmentManager.findFragmentByTag("notice2") != null)
+                            fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("notice2"));
+                        if (fragmentManager.findFragmentByTag("setting3") != null)
+                            fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("notice2"));
+                    }
+                    return true;
+
+
+                    case R.id.notice2: {
+                        if (fragmentManager.findFragmentByTag("notice2") != null)
+                            fragmentManager.beginTransaction().show(fragmentManager.findFragmentByTag("notice2")).commit();
+                        else
+                            fragmentManager.beginTransaction().add(R.id.main_frame, new Fragment1(), "notice2").commit();
+                        if (fragmentManager.findFragmentByTag("search1") != null)
+                            fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("notice2"));
+                        if (fragmentManager.findFragmentByTag("setting3") != null)
+                            fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("notice2"));
                         return true;
-                    case R.id.notice2:
-                        fragmentManager.beginTransaction()
-                                .replace(R.id.main_frame, fragment2).commit();
+
+                    }
+
+                    case R.id.setting3: {
+                        if (fragmentManager.findFragmentByTag("setting3") != null)
+                            fragmentManager.beginTransaction().show(fragmentManager.findFragmentByTag("setting3")).commit();
+                        else
+                            fragmentManager.beginTransaction().add(R.id.main_frame, new Fragment1(), "setting3").commit();
+                        if (fragmentManager.findFragmentByTag("search1") != null)
+                            fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("notice2"));
+                        if (fragmentManager.findFragmentByTag("notice2") != null)
+                            fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("notice2"));
                         return true;
-                    case R.id.setting3:
-                        fragmentManager.beginTransaction()
-                                .replace(R.id.main_frame, fragment3).commit();
-                        return true;
+
+                    }
+
                 }
                 return false;
             }
