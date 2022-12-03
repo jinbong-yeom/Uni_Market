470f49f0b874d12b3c35b48725c23a7624d2f18f
jinbong <wlftj13@naver.com>
2022-12-03 16:43:26 +0900

    리스트 갱신 추가 수정

---
+++ monitor.py
@@ -0,0 +1,31 @@
+from pymongo import MongoClient 
+import json 
+from threading import Thread
+uri = "" % (
+                    '', '', '')
+client=MongoClient(uri)
+while(1):
+    db=client['UniMarketDB']
+    collection=db['data']
+    data=[]
+    sanghan=300000
+    hahan=0
+    title="갤럭시"
+    one="S21"
+    two="사고"
+    three="무고"
+    f=open('./item_id_list.txt','r')
+    while True:
+        line=f.readline().rstrip()
+        if not line:break
+        data.append(line)
+    for i in collection.find({'$and':[{'$and':[{"price":{"$lte":sanghan}},{"price":{"$gte":hahan}},
+    {"title":{"$regex":".*{}.*".format(title)}}]},{'$nor':[{"title":{"$regex":".*{}.*".format(one)}},
+    {"title":{"$regex":".*{}.*".format(two)}},{"title":{"$regex":".*{}.*".format(three)}}]}]}):#갱신된 부분이 있는지 확인
+        if i['item_id'] not in data:
+                print('알림')
+                # 알림 보내기
+                with open('item_id_list.txt','a',encoding='UTF-8')as f:#알림 보내고 리스트 파일 갱신
+                    f.write(i['item_id']+'\n')
+    
+    break
