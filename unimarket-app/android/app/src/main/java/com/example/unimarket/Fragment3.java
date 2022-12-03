19043beeb985e11aea5fda4b41761aae1d9e9b79
15zd <mingureion@gmail.com>
2022-12-03 17:10:22 +0900

    frag3수정-루트뷰추가, 버튼 바인딩 등

--- unimarket-app/android/app/src/main/java/com/example/unimarket/Fragment3.java
+++ unimarket-app/android/app/src/main/java/com/example/unimarket/Fragment3.java
@@ -4,14 +4,22 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
 public class Fragment3 extends Fragment {
+    String regstr;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment3, container, false);
+        ViewGroup frag3V = (ViewGroup) inflater.inflate(R.layout.fragment3, container, false);
+
+        Button button1 = frag3V.findViewById(R.id.regionplus);
+
+
+
+        return frag3V;
     }
 
 }
