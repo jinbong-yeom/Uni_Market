063a01db5dcd26ce337ffd8662ab54ae9313117b
jinbong <wlftj13@naver.com>
2022-12-03 21:16:45 +0900

    통신과 결합

--- monitor.py
+++ monitor.py
@@ -1,27 +1,21 @@
+'''
 from pymongo import MongoClient 
 import json 
 from threading import Thread
-uri = "" % (
-                    '', '', '')
+uri = "mongodb://%s:%s@%s/?authMechanism=DEFAULT&authSource=UniMarketDB" % (
+                'uni', 'uni1234', 'db.yoonleeverse.com')
 client=MongoClient(uri)
-def monitor(instance):
+def monitor(title,Max,Min,Filter):
     db=client['UniMarketDB']
     collection=db['data']
     data=[]
-    sanghan=300000
-    hahan=0
-    title="갤럭시"
-    one="S21"
-    two="사고"
-    three="무고"
     f=open('./item_id_list.txt','r')
     while True:
         line=f.readline().rstrip()
         if not line:break
         data.append(line)
-    for i in collection.find({'$and':[{'$and':[{"price":{"$lte":sanghan}},{"price":{"$gte":hahan}},
-    {"title":{"$regex":".*{}.*".format(title)}}]},{'$nor':[{"title":{"$regex":".*{}.*".format(one)}},
-    {"title":{"$regex":".*{}.*".format(two)}},{"title":{"$regex":".*{}.*".format(three)}}]}]}):#갱신된 부분이 있는지 확인
+    for i in collection.find({'$and':[{'$and':[{"price":{"$lte":Max}},{"price":{"$gte":Min}},
+    {"title":{"$regex":".*{}.*".format(title)}}]},{'$nor':[{"title":{"$regex":".*{}.*".format(Filter)}}]}]}):#갱신된 부분이 있는지 확인
         if i['item_id'] not in data:
                 print('알림')
                 # 알림 보내기
@@ -30,3 +24,4 @@ def monitor(instance):
 #if 모니터링 요청이 오면: 
 thread = Thread(target=monitor, args=(), daemon=True)
 thread.start()
+'''
