78b1a3f72a1e4c8aeec3931ad75d9b401d4246bb
15zd <mingureion@gmail.com>
2022-12-04 13:00:05 +0900

    FIX: 탭 이동해도 replace안되도록 수정 #87

--- unimarket-app/android/app/src/main/java/com/example/unimarket/MainActivity.java
+++ unimarket-app/android/app/src/main/java/com/example/unimarket/MainActivity.java
@@ -6,13 +6,15 @@ import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.Toast;
+
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Fragment1 fragment1;
-//    Fragment2 fragment2;
-//    Fragment3 fragment3;
+     Fragment1 fragment1;
+     Fragment2 fragment2;
+     Fragment3 fragment3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,47 +38,50 @@ public class MainActivity extends AppCompatActivity {
                 switch (item.getItemId()) {
                     case R.id.search1: { //검색 탭이 선택됬을 때
                         // 프래그먼트가 이미 존재하면 보여주고 아니면 프래그먼트 매니저에 추가
-                        if (fragmentManager.findFragmentByTag("search1") != null)
-                            fragmentManager.beginTransaction().show(fragmentManager.findFragmentByTag("search1")).commit();
-                        else
-                            fragmentManager.beginTransaction().add(R.id.main_frame, new Fragment1(), "search1").commit();
-                        //나머지 프래그먼트가 활성화 됬을 때 가려준다.
-                        if (fragmentManager.findFragmentByTag("notice2") != null)
-                            fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("notice2"));
-                        if (fragmentManager.findFragmentByTag("setting3") != null)
-                            fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("notice2"));
-                    }
-                    return true;
+                        if (fragment1 == null) {
+                            fragment1 = new Fragment1();
+                            fragmentManager.beginTransaction().add(R.id.main_frame, fragment1).commit();
+                            Toast.makeText(getApplicationContext(), "frag1add", Toast.LENGTH_LONG).show();
+                        }
+                        if (fragment1 != null) fragmentManager.beginTransaction().show(fragment1).commit();
 
+                        //나머지 프래그먼트가 활성화 됬을 때 가려준다.
+                        if (fragment2 != null) fragmentManager.beginTransaction().hide(fragment2).commit();
+                        if (fragment3 != null) fragmentManager.beginTransaction().hide(fragment3).commit();
+
+                        return true;
+                    }
 
                     case R.id.notice2: {
-                        if (fragmentManager.findFragmentByTag("notice2") != null)
-                            fragmentManager.beginTransaction().show(fragmentManager.findFragmentByTag("notice2")).commit();
-                        else
-                            fragmentManager.beginTransaction().add(R.id.main_frame, new Fragment1(), "notice2").commit();
-                        if (fragmentManager.findFragmentByTag("search1") != null)
-                            fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("notice2"));
-                        if (fragmentManager.findFragmentByTag("setting3") != null)
-                            fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("notice2"));
+                        if (fragment2 == null) {
+                            fragment2 = new Fragment2();
+                            fragmentManager.beginTransaction().add(R.id.main_frame, fragment2).commit();
+                            Toast.makeText(getApplicationContext(), "frag2add", Toast.LENGTH_LONG).show();
+                        }
+                        if (fragment2 != null) fragmentManager.beginTransaction().show(fragment2).commit();
 
+                        if (fragment1 != null) fragmentManager.beginTransaction().hide(fragment1).commit();
+                        if (fragment3 != null) fragmentManager.beginTransaction().hide(fragment3).commit();
+
+                        return true;
                     }
-                    return true;
 
 
                     case R.id.setting3: {
-                        if (fragmentManager.findFragmentByTag("setting3") != null)
-                            fragmentManager.beginTransaction().show(fragmentManager.findFragmentByTag("setting3")).commit();
-                        else
-                            fragmentManager.beginTransaction().add(R.id.main_frame, new Fragment1(), "setting3").commit();
-                        if (fragmentManager.findFragmentByTag("search1") != null)
-                            fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("notice2"));
-                        if (fragmentManager.findFragmentByTag("notice2") != null)
-                            fragmentManager.beginTransaction().hide(fragmentManager.findFragmentByTag("notice2"));
-                    }
-                    return true;
+                        if (fragment3 == null) {
+                            fragment3 = new Fragment3();
+                            fragmentManager.beginTransaction().add(R.id.main_frame, fragment3).commit();
+                            Toast.makeText(getApplicationContext(), "frag3add", Toast.LENGTH_LONG).show();
+                        }
+                        if (fragment3 != null) fragmentManager.beginTransaction().show(fragment3).commit();
 
+                        if (fragment1 != null) fragmentManager.beginTransaction().hide(fragment1).commit();
+                        if (fragment2 != null) fragmentManager.beginTransaction().hide(fragment2).commit();
+
+                        return true;
+                    }
+                    default:return false;
                 }
-                return false;
             }
         });
     }
