40040d07ca238c0c05f3993e37e89b5b66d9e0f6
jinbong <wlftj13@naver.com>
2022-12-04 15:34:46 +0900

    알람추가1

--- monitor.py
+++ monitor.py
@@ -2,8 +2,9 @@ from pymongo import MongoClient
 import json 
 from threading import Thread
 import time
-uri = "mongodb://%s:%s@%s/?authMechanism=DEFAULT&authSource=UniMarketDB" % (
-                'uni', 'uni1234', 'db.yoonleeverse.com')
+import send
+uri = "" % (
+                '', '', '')
 client=MongoClient(uri)
 def monitor(user,title,Max,Min,Filter,region):
     db=client['UniMarketDB']
@@ -18,11 +19,10 @@ def monitor(user,title,Max,Min,Filter,region):
     {"title":{"$regex":".*{}.*".format(title)}}]},{'$nor':[{"title":{"$regex":".*{}.*".format(Filter)}},
     {"region":{"$regex":".*{}.*".format(region)}}]}]}):#갱신된 부분이 있는지 확인
         if i['item_id'] not in data:
-                print('알림')
+                send(user,i['title'])
                 # 알림 보내기
                 with open('item_id_list.txt','a',encoding='UTF-8')as f:#알림 보내고 리스트 파일 갱신
                     f.write(i['item_id']+'\n')
-#if 모니터링 요청이 오면: 
 thread = Thread(target=monitor, args=(), daemon=True)
 thread.start()
 time.sleep(3)
