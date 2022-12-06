b746731eb5d4e96562c75d3b887ef6e11997ece5
jinbong <wlftj13@naver.com>
2022-12-06 21:54:33 +0900

    모니터링 수정1

--- server/monitor.py
+++ server/monitor.py
@@ -7,17 +7,19 @@ import os
 uri = "mongodb://%s:%s@%s/?authMechanism=DEFAULT&authSource=UniMarketDB" % (
                 'uni', 'uni1234', 'db.yoonleeverse.com')
 client=MongoClient(uri)
-def monitor(user,title,Max,Min,Filter,region):
+def monitor():
     db=client['UniMarketDB']
     collection=db['data']
-    monitor_collection=db["{}".format(user)]
-    data=[]
-    for i in monitor_collection.find():
-        data.append(i)
-    for i in collection.find({'$and':[{'$and':[{"price":{"$lte":Max}},{"price":{"$gte":Min}},
-    {"title":{"$regex":".*{}.*".format(title)}}]},{'$nor':[{"title":{"$regex":".*{}.*".format(Filter)}},
-    {"region":{"$regex":".*{}.*".format(region)}}]}]}):#갱신된 부분이 있는지 확인
-        if i['item_id'] not in data:
-                send(user,i['title'])
-                # 알림 보내기
-                monitor_collection.insert_one(i['item_id'])
+    collection2=db['UserDB']
+    for post in collection2.find():
+        monitor_collection=db["{}".format(post['user_id'])]
+        data=[]
+        for i in monitor_collection.find():
+            data.append(i)
+        for i in collection.find({'$and':[{'$and':[{"price":{"$lte":post['max_price']}},{"price":{"$gte":post['min_price']}},
+        {"title":{"$regex":".*{}.*".format(post['title'])}}]},{'$nor':[{"title":{"$regex":".*{}.*".format(post['filter_keyword'])}},
+        {"region":{"$regex":".*{}.*".format(post['region'])}}]}]}):#갱신된 부분이 있는지 확인
+            if i['item_id'] not in data:
+                    send(post['user_id'],i['title'])
+                    # 알림 보내기
+                    monitor_collection.insert_one(i['item_id'])
