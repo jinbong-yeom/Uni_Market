143adefdfa63bfe264a723f151520c6445eb911e
jinbong <wlftj13@naver.com>
2022-12-06 22:09:26 +0900

    firebase id 추가1

--- server/monitor.py
+++ server/monitor.py
@@ -8,18 +8,19 @@ uri = "mongodb://%s:%s@%s/?authMechanism=DEFAULT&authSource=UniMarketDB" % (
                 'uni', 'uni1234', 'db.yoonleeverse.com')
 client=MongoClient(uri)
 def monitor():
-    db=client['UniMarketDB']
-    collection=db['data']
-    collection2=db['UserDB']
-    for post in collection2.find():
-        monitor_collection=db["{}".format(post['user_id'])]
-        data=[]
-        for i in monitor_collection.find():
-            data.append(i)
-        for i in collection.find({'$and':[{'$and':[{"price":{"$lte":post['max_price']}},{"price":{"$gte":post['min_price']}},
-        {"title":{"$regex":".*{}.*".format(post['title'])}}]},{'$nor':[{"title":{"$regex":".*{}.*".format(post['filter_keyword'])}},
-        {"region":{"$regex":".*{}.*".format(post['region'])}}]}]}):#갱신된 부분이 있는지 확인
-            if i['item_id'] not in data:
-                    send(post['user_id'],i['title'])
-                    # 알림 보내기
-                    monitor_collection.insert_one(i['item_id'])
+    while(True):
+        db=client['UniMarketDB']
+        collection=db['data']
+        collection2=db['UserDB']
+        for post in collection2.find():
+            monitor_collection=db["{}".format(post['user_id'])]
+            data=[]
+            for i in monitor_collection.find():
+                data.append(i)
+            for i in collection.find({'$and':[{'$and':[{"price":{"$lte":post['max_price']}},{"price":{"$gte":post['min_price']}},
+            {"title":{"$regex":".*{}.*".format(post['title'])}}]},{'$nor':[{"title":{"$regex":".*{}.*".format(post['filter_keyword'])}},
+            {"region":{"$regex":".*{}.*".format(post['region'])}}]}]}):#갱신된 부분이 있는지 확인
+                if i['item_id'] not in data:
+                        send(post['firebase_id'],i['title'])
+                        # 알림 보내기
+                        monitor_collection.insert_one(i['item_id'])
