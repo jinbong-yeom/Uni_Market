730bcf1864c060881b56641a58b381e4c32b5e3b
jinbong <wlftj13@naver.com>
2022-12-08 16:57:25 +0900

    삭제 함수 수정

--- server/deletedata.py
+++ server/deletedata.py
@@ -4,6 +4,12 @@ import os
 uri = "mongodb://%s:%s@%s/?authMechanism=DEFAULT&authSource=UniMarketDB" % (
                 'uni', 'uni1234', 'db.yoonleeverse.com')
 client=MongoClient(uri)
-def deletedata(user,title,Max,Min,Filter,region):
+def deletedata(user):
       db=client['UniMarketDB']
-      collection=db['data']
+      collection=db['UserDB']
+      collection2=db["{}".format(user)]
+      collection2.drop()
+      for i in collection.find({"userId":{"$regex":".*{}.*".format(user)}}):
+        collection.delete_one(i)
+
+
